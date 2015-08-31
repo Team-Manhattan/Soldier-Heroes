e08cef9e2f22e3107692136534680e8d440e279f
M-Yankov <pointblank12345@abv.bg>
2015-08-31 20:25:40 +0300

    always show login form (include press F5)

--- main.js
+++ main.js
@@ -4,22 +4,7 @@ import info from 'Modules/loadUserInfo.js';
 import factoryPlayer from 'Libraries/player.js';
 import shop from 'Modules/shop.js';
 
-let storage = window.localStorage;
-
-/*last login data-id is stored in window.localStorage. if last time is logged out
-localStore should be empty and login form must appear, else show data from last logged user*/
-
-if (storage.getItem('loggedUser') !== null) {
-    /*load module2 - shop*/
-    let player = factoryPlayer.getPlayer().createPlayer('PeshoBirata89', 'PEepi89YYY', 1, 1, 250, new Date(2013, 3, 23), []);
-    info.showLoggedUserInfo(player);
-    shop.loadShop(player);
-    /**
-     * let idOfLoggedUser = storage.getItem('loggedUser');
-     * let user = getRequestFromDBByID(idOfLoggedUser);*/
-} else {
     logFunc.log();
-}
 
 export default function init() {
 
